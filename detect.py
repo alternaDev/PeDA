37ecbb7e3819942327520ebf799380cd9c490120
Jan-Henrik Bruhn <jh.bruhn@me.com>
2017-06-30 11:42:28 +0200

    Less sleep

--- detect.py
+++ detect.py
@@ -89,7 +89,7 @@ def image_taker(queue):
             print("Queue Size: %d" % queue.qsize())
         #  cv2.imwrite(targetFolder + "/current.png", orig)
 
-        time.sleep(0.1)
+        time.sleep(0.01)
 
 
 def image_analyzer(queue, targetFolder):
